8ca92b951a336d17ebab635ded57274074ed5c46
Rakibul Hassan Rony <2018000000012@seu.edu.bd>
2021-10-19 10:25:23 +0600

    changed in main function java file

--- src/main/java/com/rony/notepadbackend/NotepadBackendApplication.java
+++ src/main/java/com/rony/notepadbackend/NotepadBackendApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class NotepadBackendApplication {
-
+//changing something
     public static void main(String[] args) {
         SpringApplication.run(NotepadBackendApplication.class, args);
     }
